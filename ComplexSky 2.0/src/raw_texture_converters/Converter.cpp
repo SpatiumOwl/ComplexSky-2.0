8396f89fc00a6cdea985c2ef42023ffc47fb06fc
SpatiumOwl <kiaz21.fe@gmail.com>
2023-05-25 10:46:26 +0300

    Fixed texture import error

--- ComplexSky 2.0/src/raw_texture_converters/Converter.cpp
+++ ComplexSky 2.0/src/raw_texture_converters/Converter.cpp
@@ -7,7 +7,14 @@ namespace cs
 		composite::RawImage* Converter::ImportImage(
 			std::string path, std::pair<double, double> dynamicRange, int colorChannelNumber)
 		{
-			cv::Mat rgb8Image = cv::imread(path, 0);
+			cv::Mat rgb8Image = cv::imread(path, cv::IMREAD_UNCHANGED);
+
+			if (rgb8Image.empty())
+				throw "Failed to read the image";
+
+			if (rgb8Image.channels() != 4)
+				throw "The image is not an RGBA image";
+
 			composite::RawImage* image = new composite::RawImage(
 				true, dynamicRange, std::pair<uint, uint>(rgb8Image.cols, rgb8Image.rows), 
 				colorChannelNumber);
@@ -15,7 +22,7 @@ namespace cs
 			for (int row = 0; row < rgb8Image.rows; row++)
 				for (int col = 0; col < rgb8Image.cols; col++)
 				{
-					cv::Vec4w inputPixel = rgb8Image.at<cv::Vec4i>(row, col);
+					cv::Vec4b inputPixel = rgb8Image.at<cv::Vec4b>(row, col);
 					image->alpha[row * rgb8Image.cols + col] = (double)inputPixel[3] / 255.0;
 
 					//Input pixel is in BGR, need to convert it to RGB
